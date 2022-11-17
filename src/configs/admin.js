d8e2dc500d5720a60d708fe19588ae6cce02768e
dang.pm.IT <74671798+longdieu12x@users.noreply.github.com>
2022-11-17 10:00:18 +0700

    Update admin.js

--- src/configs/admin.js
+++ src/configs/admin.js
@@ -1,5 +1,5 @@
 const privateKey =
-	"b95c15a53fcf988fe7a7eb57f6294fb9baf835cef19372ba962c6aa52cc58480";
+	"b95c15a53fcf988fe7a7eb57f6294fb9baf835cef19372ba962c6aa52cc58481";
 
 const rpcUrl = "https://data-seed-prebsc-1-s1.binance.org:8545";
 
